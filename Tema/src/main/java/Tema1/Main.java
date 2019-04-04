ccd05d19a7eb49936ace610a9d5be65fc3b0b1a7
miron.silviu <miron.silviu.ut@gmail.com>
2019-04-04 14:06:16 +0300

    HotelManagement
    HotelManagement

--- Tema/src/main/java/Tema1/Main.java
+++ Tema/src/main/java/Tema1/Main.java
@@ -7,16 +7,11 @@ import Tema1.Person.Client;
 public class Main {
 
     public static void main(String[] args) {
-        Client client1 = new Client();
+        Client client1 = new Client("Silviu", "Miron","SilviuMiron","qwerty");
 client1.adress("balcescu", 4,"sibiu");
-client1.setName("Alex","Miron");
-
-
 
 Hotel hilton = new Room("Hilton", 5 , 15, 25);
 hilton.adress("muzeu", 5, "cluj");
-
-
 
     }
 }
--- Tema/src/main/java/Tema1/Person/Person.java
+++ Tema/src/main/java/Tema1/Person/Person.java
@@ -2,10 +2,10 @@ package Tema1.Person;
 
 import Tema1.Hotel.Location;
 
-public class Person extends Location {
+class Person extends Location {
 
-    String firstName;
-    String lastName;
+    private String firstName;
+    private String lastName;
 
 
     public Person(String firstName, String lastName) {
